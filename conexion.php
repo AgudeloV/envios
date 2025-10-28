eb0eae163285af4729df7737ccd31219dc678f15
AgudeloV <jhonfreddyagudelojimenes01@gmail.com>
2025-10-28 10:26:32 -0500

    cambio 2

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("mysql-agudelo.alwaysdata.net", "agudelo", "", "agudelo_envios");
+$conexion = new mysqli("mysql-agudelo.alwaysdata.net", "agudelo", "agudelo_envios");
 
 if($conexion->connect_error){
     die("Error de conexión: " . $conexion->connect_error);
